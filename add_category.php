98204914efec412f3046ebe7b54e82aa860b2358
Pobarul-islam <pobarul3@gmail.com>
2024-04-27 20:43:22 +0600

    updated

--- add_category.php
+++ add_category.php
@@ -1,18 +1,4 @@
-<?php
-
-$host_name = "localhost";
-$db_name = "store_db";
-$user_name = "root";
-$password = '';
-
-$conn = new mysqli($host_name,  $user_name, $password, $db_name,);
-
-if ($conn->connect_error) {
-    echo 'Connection failed: ' . $conn->connect_error;
-}
-
-?>
-
+<?php require('./connection.php') ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,11 +12,10 @@ if ($conn->connect_error) {
 <body>
     <?php
 
-    if (isset($_GET['catagory_name'])) {
+    if (isset($_POST['catagory_name'])) {
 
-        $catagory_name =  $_GET['catagory_name'];
-        $catagory_entry_date
-            = $_GET['catagory_entry_date'];
+        $catagory_name =  $_POST['catagory_name'];
+        $catagory_entry_date = $_POST['catagory_entry_date'];
 
         $sql = "INSERT INTO  catagory (catagory_name, catagory_entry_date) VALUES('$catagory_name', '$catagory_entry_date')";
 
@@ -45,12 +30,11 @@ if ($conn->connect_error) {
 
     ?>
 
-    <form action="add_category.php" method="get">
+    <form action="add_category.php" method="POST">
         Category: <br>
         <input type="text" name="catagory_name"> <br> <br>
         Category Entry Date: <br>
-        <input type="date" name="catagory_entry_date
-"> <br> <br>
+        <input type="date" name="catagory_entry_date"> <br> <br>
         <input type="submit" value="submit">
     </form>
 
---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$host_name = "localhost";
+$db_name = "store_db";
+$user_name = "root";
+$password = '';
+
+$conn = new mysqli($host_name,  $user_name, $password, $db_name,);
+
+if ($conn->connect_error) {
+    echo 'Connection failed: ' . $conn->connect_error;
+}else{
+    echo 'Connection ok';
+}
